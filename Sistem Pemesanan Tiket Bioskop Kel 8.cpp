2dfa5ab4b3dc325d5205293ff7f4f59d5b73fac8
kemasmalfath <alfathisk1453@gmail.com>
2024-06-11 19:57:07 +0700

    menambahkan divide and qounquer

--- Sistem Pemesanan Tiket Bioskop Kel 8.cpp
+++ Sistem Pemesanan Tiket Bioskop Kel 8.cpp
@@ -45,3 +45,27 @@ void cetakAntrian(queue<T> q) {
     }
     cout << endl;
 } 
+            arr[k] = R[j];
+            ++j;
+        }
+        ++k;
+    }
+    while (i < n1) {
+        arr[k] = L[i];
+        ++i;
+        ++k;
+    }
+    while (j < n2) {
+        arr[k] = R[j];
+        ++j;
+        ++k;
+    }
+}
+
+void mergeSort(vector<int>& arr, int l, int r) {
+    if (l >= r) return;
+    int m = l + (r - l) / 2;
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
+}
