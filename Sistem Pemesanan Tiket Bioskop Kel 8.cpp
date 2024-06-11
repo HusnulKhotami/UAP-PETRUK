ea9569960ea048fade89172ceb5d0e6f23cc1787
Kemas_M_Alfath <153353493+kemasmalfath@users.noreply.github.com>
2024-06-11 20:09:08 +0700

    memperbaiki divide and qounquer

--- Sistem Pemesanan Tiket Bioskop Kel 8.cpp
+++ Sistem Pemesanan Tiket Bioskop Kel 8.cpp
@@ -45,6 +45,21 @@ void cetakAntrian(queue<T> q) {
     }
     cout << endl;
 } 
+
+void merge(vector<int>& arr, int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    vector<int> L(n1), R(n2);
+    for (int i = 0; i < n1; ++i)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; ++j)
+        R[j] = arr[m + 1 + j];
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            ++i;
+        } else {
             arr[k] = R[j];
             ++j;
         }
@@ -61,6 +76,7 @@ void cetakAntrian(queue<T> q) {
         ++k;
     }
 }
+
 
 void mergeSort(vector<int>& arr, int l, int r) {
     if (l >= r) return;
